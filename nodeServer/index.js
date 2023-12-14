80c2d95c3d4890740b5ff0032901303e654f2447
Mr-ProgramerA <65912700+Mr-ProgramerA@users.noreply.github.com>
2023-12-14 23:53:54 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,7 +3,8 @@
 const io = require("socket.io")(3000, {
   cors: {
    // origin: ["http://127.0.0.1:5500"],
-   origin: ["https://mr-programera.github.io/Node-ChatApp/frontEnd/"],
+  // origin: ["https://mr-programera.github.io/Node-ChatApp/frontEnd/"],
+   origin: ["https://node-chatapp-4u6v.onrender.com/"],
   },
 });
 
